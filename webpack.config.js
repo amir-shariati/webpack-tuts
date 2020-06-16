3480ad2dbe9811258ca79fd38826a437a7403b98
amir-shariati <seyed.amir.shariati@gmail.com>
2020-06-16 14:52:39 +0430

    style-loader and css-loader is added to module in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,13 @@ module.exports = {
         path: __dirname + '/dist',
         filename: "output.js"
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
     mode: "development"
 };
