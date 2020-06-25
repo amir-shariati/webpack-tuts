e424f5c24f3b078af426284eb1829fc43e4f9ee3
amir-shariati <seyed.amir.shariati@gmail.com>
2020-06-25 23:40:35 +0430

    webpack config is separated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-console.log('webpack...');
-console.log('webpack 2...');
-console.log('webpack 3...');
-console.log('webpack 4...');
+// require('./index.css')
+//
+// console.log('webpack...');
+// console.log('webpack 2...');
+// console.log('webpack 3...');
+// console.log('webpack 4...');
 
+class TestClass {
+    write(){
+        console.log('this is write method from test class');
+    }
+}
+
+const t = new TestClass();
+t.write();
---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const babelMinify = require('babel-minify-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        main: './src/main.js',
+    },
+    output: {
+        path:path.resolve(__dirname, './dist'),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 21000,
+                            name: 'images/[name].[hash].[ext]'
+                        }
+                    },
+                    "image-webpack-loader"
+                    ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        // new babelMinify()
+    ],
+    mode: "development",
+    devtool: 'inline-source-map'
+};
